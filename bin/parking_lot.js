2a9a46e13e02abb3d2a2588ad0c2e5b1fd407a3b
Mochammad Fadholi <mochammad.fadholi.st@gmail.com>
2020-05-13 03:53:53 +0700

    fead: add single command execution consist of exit, help, status and default

--- bin/parking_lot.js
+++ bin/parking_lot.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
-var readline = require('readline');
+const readline = require('readline');
+const Table = require('cli-table');
 
-var rl = readline.createInterface({
+const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: false
@@ -11,8 +12,62 @@ var rl = readline.createInterface({
 rl.on('line', function(cmd) {
     const spliData = cmd.split(' ');
     if (spliData.length === 1) {
-        console.log('The command is 1 argument ' + spliData);
+        checkSingleArgument(spliData[0])
     } else {
         console.log('The command more than 1 arguments ' + spliData);
     }
 });
+
+function tableHelp() {
+    const dataHelp = [
+        ['create_parking_lot $total', 'for creating total of area park'],
+        ['park $plat-number $color', 'for parking new vehicle'],
+        ['leave $number', 'for leaveing vehicle to the park'],
+        ['registration_numbers_for_cars_with_colour $color', 'for checking registration number of car based on color'],
+        ['slot_numbers_for_cars_with_colour $color', 'for checking list slot number of cars in the park using color'],
+        ['slot_number_for_registration_number $plat-number ', 'for checking number slot of cars in the park using plat-number'],
+        ['help', 'for showing list of command provided'],
+        ['status', 'for checking current slot of park'],
+        ['exit', 'for exit the program'],
+    ]
+    const table = new Table({
+        head: ['Command', 'Description'],
+        colWidths: [60, 80]
+    });
+    table.push(
+        ...dataHelp
+    );
+    console.log(table.toString());
+}
+
+function tableStatus() {
+    const dataStatus = [
+        ['1', 'BB-66-77-II', 'Red'],
+    ]
+    const table = new Table({
+        head: ['Slot no.', 'Registration no.', 'Colour'],
+        colWidths: [20, 40, 30]
+    });
+    table.push(
+        ...dataStatus
+    );
+    console.log(table.toString());
+}
+
+function checkSingleArgument(data) {
+
+    switch (data) {
+        case 'status':
+            tableStatus();
+            break;
+        case 'help':
+            tableHelp();
+            break;
+        case 'exit':
+            console.log('Goodbye...!');
+            rl.close();
+            break;
+        default:
+            console.log('Command is not found please check list command using "help"');
+    }
+}
